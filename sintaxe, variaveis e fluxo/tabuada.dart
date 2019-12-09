7d220e0ada62ffe4e1899beb66d4885151c5132a
Luis Henrique Franco <luishbfranco@hotmail.com>
2019-12-09 17:06:09 -0300

    aula sobre repetições com repetições

---
+++ sintaxe, variaveis e fluxo/tabuada.dart
@@ -0,0 +1,8 @@
+void main(){
+
+  for(int multiplicando = 1; multiplicando <= 10; multiplicando++){
+    for(int contador = 1; contador <= 10; contador++){
+      print("$multiplicando * $contador = ${multiplicando * contador}");
+    }
+  }
+}
