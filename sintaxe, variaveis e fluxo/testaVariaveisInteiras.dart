443fd13a6231a79c006bb7c73031aef636c89645
Luis Henrique Franco <luishbfranco@hotmail.com>
2019-12-09 16:28:33 -0300

    aula sobre variaveis inteiras

---
+++ sintaxe, variaveis e fluxo/testaVariaveisInteiras.dart
@@ -0,0 +1,9 @@
+void main(){
+  int idade = 19;
+  int idadeIrmao = idade - 4;
+  int idadeAvo = (idade + idadeIrmao) * 3;
+
+  print("eu tenho $idade anos");
+  print("meu irmao tem $idadeIrmao anos");
+  print("meu avo tem $idadeAvo anos");
+}
